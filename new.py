a180cc4f00aab7dd07fe594c37822924714697ff
Zorkijvolk <104836702+Zorkijvolk@users.noreply.github.com>
2025-02-11 17:45:02 +0300

    Create new.py

---
+++ new.py
@@ -0,0 +1,9 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import Qt
+
+
+class main(QMainWindow):
+    def __int__(self):
+
+        self.p = 
