8a565bcdaa331c21dd81df433da31d61948243ae
MaynkunGray <148556008+MaynkunGray@users.noreply.github.com>
2025-02-11 17:47:58 +0300

    Delete new.py

--- new.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
-from PyQt6.QtGui import QPixmap
-from PyQt6.QtCore import Qt
-
-
-class main(QMainWindow):
-    def __int__(self):
-
-        self.p = 
